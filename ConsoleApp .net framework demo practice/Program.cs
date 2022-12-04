8fbd2a1fdc92916b3ad13c2145813dad3f0a5431
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-04 21:15:43 +0600

    Add project files.

---
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.net_framework_demo_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Wrold!");
+            Console.ReadKey();
+        }
+    }
+}
