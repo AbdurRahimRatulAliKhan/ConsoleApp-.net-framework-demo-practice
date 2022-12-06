dfa3f98a72cab18614664f456203b32ea8610ad2
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-07 00:46:25 +0600

    read int numbers in C# from the keyword

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -10,9 +10,13 @@ namespace ConsoleApp.net_framework_demo_practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the name: ");
-            string name = Console.ReadLine();
-            Console.WriteLine($"Hi{name}");
+            Console.WriteLine("Enter two integer: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = number1 + number2;
+            Console.WriteLine("sum: " + result);
+            Console.WriteLine($"Sum = {number1 + number2}");
             Console.ReadKey();
         }
     }
