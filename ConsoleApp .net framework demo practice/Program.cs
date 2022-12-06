ef98949fc7d9df07d5a231cff2096fd3920e0853
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-07 02:28:08 +0600

    understanding of console class properties

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -10,15 +10,14 @@ namespace ConsoleApp.net_framework_demo_practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a key: ");
-            int var1 = Console.Read();
-            Console.WriteLine("ASCII value of the entered key is: " + var1);
-            
-            Console.WriteLine("Enter another key: ");
-            ConsoleKeyInfo var2 = Console.ReadKey();
-            Console.WriteLine($"\nEntered key: {var2.Key} " +
-                $"KeyChar: {var2.KeyChar} " +
-                $"ASCII: {(int)var2.KeyChar}");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Title = "Understanding Console Class";
+            Console.WriteLine("BackgroundColor: Green");
+            Console.WriteLine("ForegroundColor: Blue");
+            Console.WriteLine("Title: Understanding Console Class");
+
+            Console.Beep();
             Console.ReadKey();
         }
     }
