6d4e31a07871fca0e5a7db11dc896bcf2ee9fdd0
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-07 02:33:31 +0600

    complex understanding of console class

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -10,14 +10,25 @@ namespace ConsoleApp.net_framework_demo_practice
     {
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.Green;
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.Title = "Understanding Console Class";
-            Console.WriteLine("BackgroundColor: Green");
-            Console.WriteLine("ForegroundColor: Blue");
-            Console.WriteLine("Title: Understanding Console Class");
-
-            Console.Beep();
+            //Ask User to Enter the Employee Details
+            Console.WriteLine("Enter Employee Details");
+            Console.WriteLine("Enter Employee ID");
+            int EmployeeID = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Employee Name");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Enter Employee Salary");
+            int Salary = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Employee Address");
+            string Address = Console.ReadLine();
+            Console.WriteLine("Enter Employee Department");
+            string Department = Console.ReadLine();
+            //Display the Entered Employee Details
+            Console.WriteLine("\nEntered Employee Details are as Follows:");
+            Console.WriteLine($"Employee ID Is: {EmployeeID}");
+            Console.WriteLine($"Employee Name Is: {Name}");
+            Console.WriteLine($"Employee Salary Is: {Salary}");
+            Console.WriteLine($"Employee Address Is: {Address}");
+            Console.WriteLine($"Employee Department Is: {Department}");
             Console.ReadKey();
         }
     }
