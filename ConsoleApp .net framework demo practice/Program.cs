a9a89bd69b5658dc2941599db9fc4050844b5fa6
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-07 02:38:06 +0600

    show student marks using console class methods

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -10,25 +10,27 @@ namespace ConsoleApp.net_framework_demo_practice
     {
         static void Main(string[] args)
         {
-            //Ask User to Enter the Employee Details
-            Console.WriteLine("Enter Employee Details");
-            Console.WriteLine("Enter Employee ID");
-            int EmployeeID = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Employee Name");
+            //Ask the user to Enter Student Details
+            Console.WriteLine("Enter Student Details");
+            Console.WriteLine("Enter Registration Number");
+            int RegdNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Name");
             string Name = Console.ReadLine();
-            Console.WriteLine("Enter Employee Salary");
-            int Salary = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Employee Address");
-            string Address = Console.ReadLine();
-            Console.WriteLine("Enter Employee Department");
-            string Department = Console.ReadLine();
-            //Display the Entered Employee Details
-            Console.WriteLine("\nEntered Employee Details are as Follows:");
-            Console.WriteLine($"Employee ID Is: {EmployeeID}");
-            Console.WriteLine($"Employee Name Is: {Name}");
-            Console.WriteLine($"Employee Salary Is: {Salary}");
-            Console.WriteLine($"Employee Address Is: {Address}");
-            Console.WriteLine($"Employee Department Is: {Department}");
+            Console.WriteLine("Enter Marks of three Subjects:");
+            Console.WriteLine("Subject1");
+            int Mark1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Subject2");
+            int Mark2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Subject3");
+            int Mark3 = Convert.ToInt32(Console.ReadLine());
+            int TotalMarks = Mark1 + Mark2 + Mark3;
+            int AverageMark = TotalMarks / 3;
+            //Display the Student Details
+            Console.WriteLine("\nStudent Details are as Follows:");
+            Console.WriteLine($"Registration Number: {RegdNumber}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Total Marks : {TotalMarks}");
+            Console.WriteLine($"Average Mark: {AverageMark}");
             Console.ReadKey();
         }
     }
