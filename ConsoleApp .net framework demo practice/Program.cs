f2d16ec3be0b1a8994d918b96b105b0d86a943f3
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-09 02:23:18 +0600

    understand the numeric data types without decimal v3

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -10,26 +10,49 @@ namespace ConsoleApp.net_framework_demo_practice
     {
         static void Main(string[] args)
         {
-//            Int16 num1 = 123;
-             short num1 = 123; // 2 bytes
-//            Int32 num2 = 456;
-             int num2 = 456; // 4 bytes
-//            Int64 num3 = 789;
-             long num3 = 789; // 8 bytes
 
-            Console.WriteLine($"Min: {short.MinValue} " +
-                $"Max: {short.MaxValue} " +
-                $"Size: {sizeof(short)}");
+                            //            Int16 num1 = 123;
+            ushort num1 = 123; // 2 bytes
+                              //            Int32 num2 = 456;
+            uint num2 = 456; // 4 bytes
+                            //            Int64 num3 = 789;
+            ulong num3 = 789; // 8 bytes
 
-            Console.WriteLine($"Min: {int.MinValue} " +
-                $"Max: {int.MaxValue} " +
-                $"Size: {sizeof(int)}");
+            Console.WriteLine($"Min: {ushort.MinValue} " +
+                $"Max: {ushort.MaxValue} " +
+                $"Size: {sizeof(ushort)}");
 
-            Console.WriteLine($"Min: {long.MinValue} " +
-                $"Max: {long.MaxValue} " +
-                $"Size: {sizeof(long)}");
+            Console.WriteLine($"Min: {uint.MinValue} " +
+                $"Max: {uint.MaxValue} " +
+                $"Size: {sizeof(uint)}");
+
+            Console.WriteLine($"Min: {ulong.MinValue} " +
+                $"Max: {ulong.MaxValue} " +
+                $"Size: {sizeof(ulong)}");
 
             Console.ReadKey(true);
+
+
+            ////            Int16 num1 = 123;
+            //             short num1 = 123; // 2 bytes
+            ////            Int32 num2 = 456;
+            //             int num2 = 456; // 4 bytes
+            ////            Int64 num3 = 789;
+            //             long num3 = 789; // 8 bytes
+
+            //            Console.WriteLine($"Min: {short.MinValue} " +
+            //                $"Max: {short.MaxValue} " +
+            //                $"Size: {sizeof(short)}");
+
+            //            Console.WriteLine($"Min: {int.MinValue} " +
+            //                $"Max: {int.MaxValue} " +
+            //                $"Size: {sizeof(int)}");
+
+            //            Console.WriteLine($"Min: {long.MinValue} " +
+            //                $"Max: {long.MaxValue} " +
+            //                $"Size: {sizeof(long)}");
+
+            //            Console.ReadKey(true);
         }
     }
 }
