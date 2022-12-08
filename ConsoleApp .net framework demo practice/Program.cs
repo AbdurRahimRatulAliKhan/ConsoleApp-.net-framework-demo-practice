d9e5cb61206720bd68c8ae8f6fef26f3793c3f57
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-09 03:06:29 +0600

    numeric numbers with decimal in c# v1

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -10,27 +10,40 @@ namespace ConsoleApp.net_framework_demo_practice
     {
         static void Main(string[] args)
         {
+            Single a = 1.123F;
+            Double b = 1.456;
+            Decimal c = 1.789M;
 
-                            //            Int16 num1 = 123;
-            ushort num1 = 123; // 2 bytes
-                              //            Int32 num2 = 456;
-            uint num2 = 456; // 4 bytes
-                            //            Int64 num3 = 789;
-            ulong num3 = 789; // 8 bytes
+            Console.WriteLine($"Size: {sizeof(Single)} " +
+                $"Max: {Single.MaxValue} " +
+                $"Min: {Single.MinValue}");
+            Console.WriteLine($"Size: {sizeof(Double)} " +
+                $"Max: {Double.MaxValue} " +
+                $"Min: {Double.MinValue}");
+            Console.WriteLine($"Size: {sizeof(Decimal)} " +
+                $"Max: {Decimal.MaxValue} " +
+                $"Min: {Decimal.MinValue}");
+            Console.ReadKey();
+                                   //            Int16 num1 = 123;
+            //ushort num1 = 123;    // 2 bytes
+            //                     //            Int32 num2 = 456;
+            //uint num2 = 456;    // 4 bytes
+            //                   //            Int64 num3 = 789;
+            //ulong num3 = 789; // 8 bytes
 
-            Console.WriteLine($"Min: {ushort.MinValue} " +
-                $"Max: {ushort.MaxValue} " +
-                $"Size: {sizeof(ushort)}");
+            //Console.WriteLine($"Min: {ushort.MinValue} " +
+            //    $"Max: {ushort.MaxValue} " +
+            //    $"Size: {sizeof(ushort)}");
 
-            Console.WriteLine($"Min: {uint.MinValue} " +
-                $"Max: {uint.MaxValue} " +
-                $"Size: {sizeof(uint)}");
+            //Console.WriteLine($"Min: {uint.MinValue} " +
+            //    $"Max: {uint.MaxValue} " +
+            //    $"Size: {sizeof(uint)}");
 
-            Console.WriteLine($"Min: {ulong.MinValue} " +
-                $"Max: {ulong.MaxValue} " +
-                $"Size: {sizeof(ulong)}");
+            //Console.WriteLine($"Min: {ulong.MinValue} " +
+            //    $"Max: {ulong.MaxValue} " +
+            //    $"Size: {sizeof(ulong)}");
 
-            Console.ReadKey(true);
+            //Console.ReadKey(true);
 
 
             ////            Int16 num1 = 123;
