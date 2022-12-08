cbdc5ea2fb00f111e891cf3d265ec2f60a3d3aef
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-09 02:10:58 +0600

    understand the numeric data types without decimal

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -10,21 +10,24 @@ namespace ConsoleApp.net_framework_demo_practice
     {
         static void Main(string[] args)
         {
-            Int16 num1 = 123;
-            Int32 num2 = 456;
-            Int64 num3 = 789;
+//            Int16 num1 = 123;
+             short num1 = 123; // 2 bytes
+//            Int32 num2 = 456;
+             int num2 = 456; // 4 bytes
+//            Int64 num3 = 789;
+             long num3 = 789; // 8 bytes
 
-            Console.WriteLine($"Min: {Int16.MinValue} " +
-                $"Max: {Int16.MaxValue} " +
-                $"Size: {sizeof(Int16)}");
+            Console.WriteLine($"Min: {short.MinValue} " +
+                $"Max: {short.MaxValue} " +
+                $"Size: {sizeof(short)}");
 
-            Console.WriteLine($"Min: {Int32.MinValue} " +
-                $"Max: {Int32.MaxValue} " +
-                $"Size: {sizeof(Int32)}");
+            Console.WriteLine($"Min: {int.MinValue} " +
+                $"Max: {int.MaxValue} " +
+                $"Size: {sizeof(int)}");
 
-            Console.WriteLine($"Min: {Int64.MinValue} " +
-                $"Max: {Int64.MaxValue} " +
-                $"Size: {sizeof(Int64)}");
+            Console.WriteLine($"Min: {long.MinValue} " +
+                $"Max: {long.MaxValue} " +
+                $"Size: {sizeof(long)}");
 
             Console.ReadKey(true);
         }
