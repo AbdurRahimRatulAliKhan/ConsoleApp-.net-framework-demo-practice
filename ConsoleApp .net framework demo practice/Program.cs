2a3db02f0a7f92dbbceb6303c7f3e919f1a5323c
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-08 23:11:23 +0600

    understand numeric data types without decimal

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -10,10 +10,23 @@ namespace ConsoleApp.net_framework_demo_practice
     {
         static void Main(string[] args)
         {
-            string value = "ABC";
-            var howmanybytes = value.Length * sizeof(char);
-            Console.WriteLine($"{value} {howmanybytes}");
-            Console.ReadKey();
+            Int16 num1 = 123;
+            Int32 num2 = 456;
+            Int64 num3 = 789;
+
+            Console.WriteLine($"Min: {Int16.MinValue} " +
+                $"Max: {Int16.MaxValue} " +
+                $"Size: {sizeof(Int16)}");
+
+            Console.WriteLine($"Min: {Int32.MinValue} " +
+                $"Max: {Int32.MaxValue} " +
+                $"Size: {sizeof(Int32)}");
+
+            Console.WriteLine($"Min: {Int64.MinValue} " +
+                $"Max: {Int64.MaxValue} " +
+                $"Size: {sizeof(Int64)}");
+
+            Console.ReadKey(true);
         }
     }
 }
