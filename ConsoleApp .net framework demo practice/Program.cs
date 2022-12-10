f39fccfa9a35644dd136f6d3257bfcee67c1fae6
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-11 01:36:43 +0600

    Explicit Constructor

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -11,34 +11,46 @@ namespace ConsoleApp.net_framework_demo_practice
     {
         static void Main(string[] args)
         {
-            Test test = new Test();
-            Console.WriteLine($"i: {test.i}");
-            Console.WriteLine($"b: {test.b}");
-//            Console.WriteLine(test.s);
-            if(test.s == null)
-            {
-                Console.WriteLine($"s: Null");
-            }
+            ExplicitConstructor explicitConstructor1 = new ExplicitConstructor();
+            ExplicitConstructor explicitConstructor2 = new ExplicitConstructor();
+            ExplicitConstructor explicitConstructor3 = new ExplicitConstructor();
+            ExplicitConstructor explicitConstructor4 = new ExplicitConstructor();
             Console.ReadKey();
-            //Calculator calculator = new Calculator();
-            //int result = calculator.CalculateSum(10, 20);
-            //Console.WriteLine(result);
-            //Console.ReadKey();
+//            Test test = new Test();
+//            Console.WriteLine($"i: {test.i}");
+//            Console.WriteLine($"b: {test.b}");
+////            Console.WriteLine(test.s);
+//            if(test.s == null)
+//            {
+//                Console.WriteLine($"s: Null");
+//            }
+//            Console.ReadKey();
+//            //Calculator calculator = new Calculator();
+//            //int result = calculator.CalculateSum(10, 20);
+//            //Console.WriteLine(result);
+//            //Console.ReadKey();
         }
 
-        class Test
+        class ExplicitConstructor
         {
-            public int i;
-            public bool b;
-            public string s;
+            public ExplicitConstructor()
+            {
+                Console.WriteLine("Explicit Constructor is called!");
+            }
         }
-        //public class Calculator
+        //class Test
         //{
-        //    public int CalculateSum(int no1, int no2)
-        //    {
-        //        return no1 + no2;
-        //    }
+        //    public int i;
+        //    public bool b;
+        //    public string s;
         //}
+        ////public class Calculator
+        ////{
+        ////    public int CalculateSum(int no1, int no2)
+        ////    {
+        ////        return no1 + no2;
+        ////    }
+        ////}
 
     }
 }
