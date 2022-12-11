6e3455ee6af81c7eef3b3716c129cc2d8e8eca84
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-11 12:47:29 +0600

    example to understand user-defined default constructor in C#

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -7,50 +7,47 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp.net_framework_demo_practice
 {
-    internal class Program
+    class Employee
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Id, Age;
+        public string Address, Name;
+        public bool IsPermanent;
+        /// <summary>
+        /// 
+        /// </summary>
+        public Employee()
+        {
+            Id = 100;
+            Age = 30;
+            Address = "Dhaka";
+            Name = "John";
+            IsPermanent = true;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Display()
+        {
+            Console.WriteLine($"Id: {Id}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"Address: {Address}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Is Employee Permanent: {IsPermanent}");
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            ExplicitConstructor explicitConstructor1 = new ExplicitConstructor();
-            ExplicitConstructor explicitConstructor2 = new ExplicitConstructor();
-            ExplicitConstructor explicitConstructor3 = new ExplicitConstructor();
-            ExplicitConstructor explicitConstructor4 = new ExplicitConstructor();
-            Console.ReadKey();
-//            Test test = new Test();
-//            Console.WriteLine($"i: {test.i}");
-//            Console.WriteLine($"b: {test.b}");
-////            Console.WriteLine(test.s);
-//            if(test.s == null)
-//            {
-//                Console.WriteLine($"s: Null");
-//            }
-//            Console.ReadKey();
-//            //Calculator calculator = new Calculator();
-//            //int result = calculator.CalculateSum(10, 20);
-//            //Console.WriteLine(result);
-//            //Console.ReadKey();
-        }
+            Employee employee = new Employee();
+            employee.Display();
 
-        class ExplicitConstructor
-        {
-            public ExplicitConstructor()
-            {
-                Console.WriteLine("Explicit Constructor is called!");
-            }
+            Console.ReadKey();
         }
-        //class Test
-        //{
-        //    public int i;
-        //    public bool b;
-        //    public string s;
-        //}
-        ////public class Calculator
-        ////{
-        ////    public int CalculateSum(int no1, int no2)
-        ////    {
-        ////        return no1 + no2;
-        ////    }
-        ////}
 
     }
 }
