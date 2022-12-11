4f083fb190069900206af1c305f5bc8c8af63c82
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-12 01:19:20 +0600

    copy constructor real-time example in C#

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ConsoleApp.net_framework_demo_practice
 {
@@ -37,6 +39,19 @@ namespace ConsoleApp.net_framework_demo_practice
         /// <summary>
         /// 
         /// </summary>
+        /// <param name="FromAquaculture"></param>
+        public Employee(Employee FromAquaculture)
+        {
+            Id = FromAquaculture.Id;
+            Age = FromAquaculture.Age;
+            Address = FromAquaculture.Address;
+            Name = FromAquaculture.Name;
+            IsPermanent = FromAquaculture.IsPermanent;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
         public void Display()
         {
             Console.WriteLine($"Id: {Id}");
@@ -56,14 +71,21 @@ namespace ConsoleApp.net_framework_demo_practice
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            Console.WriteLine("Dept. of GYM");
             Employee GYM = new Employee(110111, 30, 
                 "CL(USA)", "Amir", true);
             GYM.Display();
             Console.WriteLine();
 
+            Console.WriteLine("Dept. of Ranch");
             Employee ranch = new Employee(110222, 40,
                 "Canada", "John", false);
             ranch.Display();
+            Console.WriteLine();
+
+            Console.WriteLine("Dept. of Aquaculture");
+            Employee Aquaculture = new Employee(GYM);
+            Aquaculture.Display();
             Console.ReadKey();
         }
     }
