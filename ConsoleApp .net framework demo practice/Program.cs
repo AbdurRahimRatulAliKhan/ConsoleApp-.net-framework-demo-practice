57dffd4212a9249f1f82f2dc78b66a13f3550eae
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-11 18:26:03 +0600

    copy constructor

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -11,69 +11,28 @@ namespace ConsoleApp.net_framework_demo_practice
     {
         static void Main(string[] args)
         {
-            ParameterizedConstructor parameterizedConstructor1 = 
-                new ParameterizedConstructor(10);
-            parameterizedConstructor1.Display();
-            ParameterizedConstructor parameterizedConstructor2 =
-                new ParameterizedConstructor(20);
-            parameterizedConstructor2.Display();
+            CopyConstructor copyConstructor1 = new CopyConstructor(10);
+            copyConstructor1.Display();
+            CopyConstructor copyConstructor2 = new CopyConstructor(copyConstructor1);
+            copyConstructor2.Display();
             Console.ReadKey();
         }
 
-        class ParameterizedConstructor
+        class CopyConstructor
         {
             int x;
-            public ParameterizedConstructor(int i)
+            public CopyConstructor(int i)
             {
                 x = i;
-                Console.WriteLine($"Parameterized Constructor  " +
-                    $"is called: {i}");
+            }
+            public CopyConstructor(CopyConstructor obj)
+            {
+                x = obj.x;
             }
             public void Display()
             {
-                Console.WriteLine($"Value of X is: {x}");
+                Console.WriteLine($"x: {x}");
             }
         }
-        
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        //public int Id, Age;
-        //public string Address, Name;
-        //public bool IsPermanent;
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        //public Program()
-        //{
-        //    Id = 100;
-        //    Age = 30;
-        //    Address = "Dhaka";
-        //    Name = "John";
-        //    IsPermanent = true;
-        //}
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        //public void Display()
-        //{
-        //    Console.WriteLine($"Id: {Id}");
-        //    Console.WriteLine($"Age: {Age}");
-        //    Console.WriteLine($"Address: {Address}");
-        //    Console.WriteLine($"Name: {Name}");
-        //    Console.WriteLine($"Is Employee Permanent: {IsPermanent}");
-        //}
-        ///// <summary>
-        ///// 
-        ///// </summary>
-        ///// <param name="args"></param>
-        //static void Main(string[] args)
-        //{
-        //    Program employee = new Program();
-        //    employee.Display();
-
-        //    Console.ReadKey();
-        //}
-
     }
 }
