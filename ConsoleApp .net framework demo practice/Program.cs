4f85f086b01f0cbdaf79b1bf7e62b064b77031ef
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-11 23:52:39 +0600

    Need to use explicit constructor

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -7,15 +7,26 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp.net_framework_demo_practice
 {
+    class First
+    {
+        public int x = 100;
+    }
+    class Second
+    {
+        public int x;
+    }
     class Program
     {
-        private Program()
-        {
-            Console.WriteLine("private constructor");
-        }
         static void Main(string[] args)
         {
-            Console.WriteLine("main method");
+            First first1 = new First();
+            First first2 = new First();
+            First first3 = new First();
+            Console.WriteLine($"{first1.x} {first2.x} {first3.x}");
+            Second second1 = new Second();
+            Second second2 = new Second();
+            Second second3 = new Second();
+            Console.WriteLine($"{second1.x} {second2.x} {second3.x}");
             Console.ReadKey();
         }
     }
