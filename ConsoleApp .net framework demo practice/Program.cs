cec61e003540d07804fcc22fa871687c098d6b64
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-12 00:58:37 +0600

    parameterized constructor real-time example in C#

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -10,44 +10,60 @@ namespace ConsoleApp.net_framework_demo_practice
     /// <summary>
     /// 
     /// </summary>
-    class First
+    class Employee
     {
-        public int x = 100;
-    }
-    /// <summary>
-    /// 
-    /// </summary>
-    class Second
-    {
-        public int x;
+        public int Id, Age;
+        public string Address, Name;
+        public bool IsPermanent;
+
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="x"></param>
-        public Second(int x)
+        /// <param name="id"></param>
+        /// <param name="age"></param>
+        /// <param name="address"></param>
+        /// <param name="name"></param>
+        /// <param name="isPermanent"></param>
+        public Employee(int id, int age, string address, string name, 
+            bool isPermanent)
         {
-            this.x = x;
+            Id = id;
+            Age = age;
+            Address = address;
+            Name = name;
+            IsPermanent = isPermanent;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Display()
+        {
+            Console.WriteLine($"Id: {Id}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"Address: {Address}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"IsPermanent: {IsPermanent}");
         }
     }
-    /// <summary>
-    /// 
-    /// </summary>
+
     class Program
     {
+
         /// <summary>
         /// 
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            First first1 = new First();
-            First first2 = new First();
-            First first3 = new First();
-            Console.WriteLine($"{first1.x} {first2.x} {first3.x}");
-            Second second1 = new Second(10);
-            Second second2 = new Second(20);
-            Second second3 = new Second(30);
-            Console.WriteLine($"{second1.x} {second2.x} {second3.x}");
+            Employee GYM = new Employee(110111, 30, 
+                "CL(USA)", "Amir", true);
+            GYM.Display();
+            Console.WriteLine();
+
+            Employee ranch = new Employee(110222, 40,
+                "Canada", "John", false);
+            ranch.Display();
             Console.ReadKey();
         }
     }
