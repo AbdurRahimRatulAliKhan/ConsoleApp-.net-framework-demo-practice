ec29b9d81ddb104c5db4346877623f6d31dd0892
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-11 16:37:44 +0600

    Parameterized Constructor

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -7,47 +7,65 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp.net_framework_demo_practice
 {
-    class Employee
+    class Program
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        public int Id, Age;
-        public string Address, Name;
-        public bool IsPermanent;
-        /// <summary>
-        /// 
-        /// </summary>
-        public Employee()
-        {
-            Id = 100;
-            Age = 30;
-            Address = "Dhaka";
-            Name = "John";
-            IsPermanent = true;
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        public void Display()
-        {
-            Console.WriteLine($"Id: {Id}");
-            Console.WriteLine($"Age: {Age}");
-            Console.WriteLine($"Address: {Address}");
-            Console.WriteLine($"Name: {Name}");
-            Console.WriteLine($"Is Employee Permanent: {IsPermanent}");
-        }
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Employee employee = new Employee();
-            employee.Display();
-
+            ParameterizedConstructor parameterizedConstructor1 = 
+                new ParameterizedConstructor(10);
+            ParameterizedConstructor parameterizedConstructor2 =
+                new ParameterizedConstructor(20);
             Console.ReadKey();
         }
+
+        class ParameterizedConstructor
+        {
+            public ParameterizedConstructor(int i)
+            {
+                Console.WriteLine($"Parameterized Constructor  " +
+                    $"is called: {i}");
+            }
+        }
+        
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public int Id, Age;
+        //public string Address, Name;
+        //public bool IsPermanent;
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public Program()
+        //{
+        //    Id = 100;
+        //    Age = 30;
+        //    Address = "Dhaka";
+        //    Name = "John";
+        //    IsPermanent = true;
+        //}
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        //public void Display()
+        //{
+        //    Console.WriteLine($"Id: {Id}");
+        //    Console.WriteLine($"Age: {Age}");
+        //    Console.WriteLine($"Address: {Address}");
+        //    Console.WriteLine($"Name: {Name}");
+        //    Console.WriteLine($"Is Employee Permanent: {IsPermanent}");
+        //}
+        ///// <summary>
+        ///// 
+        ///// </summary>
+        ///// <param name="args"></param>
+        //static void Main(string[] args)
+        //{
+        //    Program employee = new Program();
+        //    employee.Display();
+
+        //    Console.ReadKey();
+        //}
 
     }
 }
