351df5d45bab88e0334ceec137dc7fc584e1441b
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-13 02:25:05 +0600

    Child class is declared inside the Parent class and inheritance is possible

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -13,8 +13,9 @@ namespace ConsoleApp.net_framework_demo_practice
     {
         static void Main(string[] args)
         {
-            Parent.Child child = new Parent.Child();  
-            Console.ReadKey(true);
+            Parent.Child1 child1 = new Parent.Child1();
+            Parent.Child2 child2 = new Parent.Child2();
+            Console.ReadKey();
         }
     }
 
@@ -22,15 +23,20 @@ namespace ConsoleApp.net_framework_demo_practice
     {
         private Parent()
         {
-            Console.WriteLine("parent calss private constructor");
+            Console.WriteLine("parent class private constructor");
         }
-        
-
-        public class Child : Parent
+        public class Child1 : Parent
         {
-            public Child()
+            public Child1()
             {
-                Console.WriteLine("child class public constructor");
+                Console.WriteLine("child1 class public constructor");
+            }
+        }
+        public class Child2 : Parent
+        {
+            public Child2()
+            {
+                Console.WriteLine("child2 class public constructor");
             }
         }
     }
