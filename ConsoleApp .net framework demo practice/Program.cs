1e4123e59ebf6fbdb3e5dafaf6c83f64a8777f96
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-13 18:15:59 +0600

    singleton design pattern implementation example in c#

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -11,19 +11,42 @@ namespace ConsoleApp.net_framework_demo_practice
 {
     class Program
     {
-        private Program()
-        {
-            Console.WriteLine("Private parameterless constructor");
-        }
-        private Program(string message)
-        {
-            Console.WriteLine("Private parameterized constructor");
-        }
         static void Main(string[] args)
         {
-            Program program1 = new Program();
-            Program program2 = new Program("Hi");
-            Console.ReadKey(true);
+            Singleton singleton1 = Singleton.GetSingletonInstance();
+            singleton1.SomeMethod("from singleton1");
+            Singleton singleton2 = Singleton.GetSingletonInstance();
+            singleton2.SomeMethod("from singleton2");
+            Console.ReadKey();
+        }
+    }
+
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+        
+        private static readonly object Instancelock = new object();
+        
+        public static Singleton GetSingletonInstance()
+        {
+            lock (Instancelock)
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine($"singleton constructor executed {counter} times");
+        }
+        public void SomeMethod(string Message)
+        {
+            Console.WriteLine($"somemethod called {Message}");
         }
     }
     
