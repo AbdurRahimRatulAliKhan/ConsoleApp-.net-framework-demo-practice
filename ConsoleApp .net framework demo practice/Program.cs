0efefb1b8b36d89c5bd97b176f625436272a60cc
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-17 20:14:54 +0600

    When to use Destructor in C#

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -4,31 +4,23 @@ namespace ConsoleApp.net_framework_demo_practice
 {
     public class DestructorDemo
     {
+        public DestructorDemo()
+        {
+            Console.WriteLine("Unmanaged Resource Created");
+        }
         ~DestructorDemo()
         {
-            Console.WriteLine("Destructor of 1st called");
+            Console.WriteLine("Unmanaged Resource Distroyed");
         }
     }
-    public class DestructorDemo2 : DestructorDemo
-    {
-        ~DestructorDemo2()
-        {
-            Console.WriteLine("Destructor of 2nd called");
-        }
-    }
-    public class DestructorDemo3 : DestructorDemo2
-    {
-        ~DestructorDemo3()
-        {
-            Console.WriteLine("Destructor of 3rd called");
-        }
-    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            DestructorDemo3 destructorDemo3 = new DestructorDemo3();
-            destructorDemo3 = null;
+            DestructorDemo destructorDemo = new DestructorDemo();
+            Console.WriteLine("Using Unmanaged Resource");
+            destructorDemo = null;
             GC.Collect();
             Console.ReadKey();
         }
