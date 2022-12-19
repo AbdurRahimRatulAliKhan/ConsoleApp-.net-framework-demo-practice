81631a14ff2ba0ff0e390f9bb16c10ed2363de6a
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-19 20:00:58 +0600

    public access specifiers or access modifiers in c sharp with examples

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -1,139 +1,69 @@
-﻿using System;
+﻿using ClassLibrary1;
+using System;
 
 namespace ConsoleApp.net_framework_demo_practice
 {
-    public class MyClass1 : IDisposable
-    {
-        #region IDisposable Support
-        private bool disposedValue = false ;
-
-        //~MyClass1()
-        //{
-        //    Console.WriteLine("class1");
-        //}
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: dispose managed state (managed objects)
-                }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-                // TODO: set large fields to null
-                disposedValue = true;
-            }
-        }
-
-        // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-        ~MyClass1()
-        {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: false);
-        }
-
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
-        #endregion
-
-    }
-    public class MyClass2 : IDisposable
-    {
-        #region IDisposable Support for class2
-        private bool disposedValue = false;
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: dispose managed state (managed objects)
-                }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-                // TODO: set large fields to null
-                disposedValue = true;
-            }
-        }
-
-        // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-        ~MyClass2()
-        {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: false);
-        }
-
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
-        #endregion
-        //~MyClass2()
-        //{
-        //    Console.WriteLine("class2");
-        //}
-
-    }
-    public class MyClass3 : IDisposable
-    {
-        #region IDisposable Support for calss3
-        private bool disposedValue = false;
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: dispose managed state (managed objects)
-                }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-                // TODO: set large fields to null
-                disposedValue = true;
-            }
-        }
-
-        // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-        ~MyClass3()
-        {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: false);
-        }
-
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
-        #endregion
-        //~MyClass3()
-        //{
-        //    Console.WriteLine("class3");
-        //}
-
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i <= 1000000; i++)
-            {
-                MyClass1 myClass1= new MyClass1();
-                MyClass2 myClass2= new MyClass2();
-                MyClass3 myClass3= new MyClass3();
-            }
-            Console.Read();
+            
         }
     }
+    public class AnotherClassLibrary1Class1 : ClassLibrary1Class1
+    {
+        public void Display4()
+        {
+            Console.WriteLine(Id);
+        }
+    }
+    public class AnotherClassLibrary1Class2
+    {
+        public void Display3()
+        {
+            ClassLibrary1Class1 classLibrary1Class1 = new ClassLibrary1Class1();
+            Console.WriteLine(classLibrary1Class1.Id);
+        }
+    }
+    //public class MyClass2 : IDisposable
+    //{
+    //    #region IDisposable Support for class2
+    //    private bool disposedValue = false;
+
+    //    protected virtual void Dispose(bool disposing)
+    //    {
+    //        if (!disposedValue)
+    //        {
+    //            if (disposing)
+    //            {
+    //                // TODO: dispose managed state (managed objects)
+    //            }
+
+    //            // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+    //            // TODO: set large fields to null
+    //            disposedValue = true;
+    //        }
+    //    }
+
+    //    // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+    //    ~MyClass2()
+    //    {
+    //        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+    //        Dispose(disposing: false);
+    //    }
+
+    //    public void Dispose()
+    //    {
+    //        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+    //        Dispose(disposing: true);
+    //        GC.SuppressFinalize(this);
+    //    }
+    //    #endregion
+    //    //~MyClass2()
+    //    //{
+    //    //    Console.WriteLine("class2");
+    //    //}
+
+    //}
+
 }
