3cfeb7063aaf9c9d54f267e782d8a275db6acfc2
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-19 17:08:43 +0600

    dispose pattern to distroy the unmanaged object in c#

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -2,11 +2,15 @@
 
 namespace ConsoleApp.net_framework_demo_practice
 {
-    public class UnmanagedResource : IDisposable
+    public class MyClass1 : IDisposable
     {
         #region IDisposable Support
-//        private bool disposedValue = false;
-        private bool disposedValue;
+        private bool disposedValue = false ;
+
+        //~MyClass1()
+        //{
+        //    Console.WriteLine("class1");
+        //}
 
         protected virtual void Dispose(bool disposing)
         {
@@ -15,62 +19,121 @@ namespace ConsoleApp.net_framework_demo_practice
                 if (disposing)
                 {
                     // TODO: dispose managed state (managed objects)
-                    Console.WriteLine("4: Managed resoucrces destroyed by disposed method");
                 }
 
                 // TODO: free unmanaged resources (unmanaged objects) and override finalizer
                 // TODO: set large fields to null
-                Console.WriteLine("5: unmanaged resources destroyed by dispose method");
                 disposedValue = true;
-            }
-            else
-            {
-                Console.WriteLine("7: resources are already destroyed by dispose method");
             }
         }
 
         // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-        ~UnmanagedResource()
+        ~MyClass1()
         {
-            Console.WriteLine("request comes to destructor to destroy the resources");
             // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
             Dispose(disposing: false);
         }
 
         public void Dispose()
         {
-            Console.WriteLine("3: request comes to dispose method to destroy the resources");
             // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
             Dispose(disposing: true);
-//            GC.Collect();
             GC.SuppressFinalize(this);
         }
         #endregion
+
+    }
+    public class MyClass2 : IDisposable
+    {
+        #region IDisposable Support for class2
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        ~MyClass2()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+        //~MyClass2()
+        //{
+        //    Console.WriteLine("class2");
+        //}
+
+    }
+    public class MyClass3 : IDisposable
+    {
+        #region IDisposable Support for calss3
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        ~MyClass3()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+        //~MyClass3()
+        //{
+        //    Console.WriteLine("class3");
+        //}
+
     }
     class Program
     {
         static void Main(string[] args)
-//        static void Main()
         {
-            UnmanagedResource unmanagedResource = null;
-            try
+            for (int i = 0; i <= 1000000; i++)
             {
-                unmanagedResource = new UnmanagedResource();
-                Console.WriteLine("1: using resources");
+                MyClass1 myClass1= new MyClass1();
+                MyClass2 myClass2= new MyClass2();
+                MyClass3 myClass3= new MyClass3();
             }
-            finally
-            {
-                if (unmanagedResource != null)
-                {
-                    Console.WriteLine("2: calling dispose method to destroy resources");
-                    unmanagedResource.Dispose();
-                }
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("6: trying to call the dispose method again to destroy resources");
-            unmanagedResource.Dispose();
-            Console.ReadKey();
+            Console.Read();
         }
     }
 }
