b473b9911adaab8edabf17c56b59462f44dbae1f
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2022-12-19 19:36:08 +0600

    private access specifier or modifier in c# with example

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class ClassLibrary1Class1
+    {
+        private int Id;
+//        public int Id;
+        public void Display1()
+        {
+            Console.WriteLine(Id);
+        }
+    }
+    public class ClassLibrary1Class2 : ClassLibrary1Class1
+    {
+        public void Display2()
+        {
+            Console.WriteLine(Id);
+        }
+    }
+    public class ClassLibrary1Class3
+    {
+        public void Display3()
+        {
+            ClassLibrary1Class1 classLibrary1Class1 = new ClassLibrary1Class1();
+            Console.WriteLine(classLibrary1Class1.Id);
+        }
+    }
+}
