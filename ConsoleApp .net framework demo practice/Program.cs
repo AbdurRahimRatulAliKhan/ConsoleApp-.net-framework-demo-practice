088b39b8f1e01f78741f39595d8e3f8ad6f6b1d0
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2023-01-10 01:16:10 +0600

    v2

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -16,11 +16,16 @@ namespace ConsoleApp.net_framework_demo_practice
     }
     class Program : A
     {
+        public void Method3()
+        {
+            Console.WriteLine("Method 3");
+        }
         static void Main(string[] args)
         {
             Program program = new Program();
             program.Method1();
             program.Method2();
+            program.Method3();
             Console.Read();
         }
     }
