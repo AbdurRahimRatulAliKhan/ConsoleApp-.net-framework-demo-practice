2626b62bbe34aab1cf87bb90ec1911ba7a7ea229
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2023-01-10 00:31:27 +0600

    example to understand inheritance in C#

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -3,111 +3,24 @@ using System;
 
 namespace ConsoleApp.net_framework_demo_practice
 {
-    public abstract class IBank
+    class A
     {
-        public abstract void ValidateCard();
-        public abstract void WithdrawMoney();
-        public abstract void CheckBalance();
-        public abstract void BankTransfer();
-        public abstract void MiniStatement();
-
-    }
-
-    public class BankFactory
-    {
-        public static IBank GetBankObject(string bankType)
+        public void Method1()
         {
-            IBank BankObject = null;
-            if(bankType == "SBI")
-            {
-                BankObject = new SBI();
-            }
-            else if (bankType == "AXIX")
-            {
-                BankObject = new AXIX();
-            }
-            return BankObject;
+            Console.WriteLine("Method 1");
+        }
+        public void Method2()
+        {
+            Console.WriteLine("Method 2");
         }
     }
-    public class SBI : IBank
-    {
-        public override void BankTransfer()
-        {
-            Console.WriteLine("SBI Bank Bank Transfer");
-        }
-
-        public override void CheckBalance()
-        {
-            Console.WriteLine("SBI Bank Check Balance");
-        }
-
-        public override void MiniStatement()
-        {
-            Console.WriteLine("SBI Bank Mini Statement");
-        }
-
-        public override void ValidateCard()
-        {
-            Console.WriteLine("SBI Bank Validate Card");
-        }
-
-        public override void WithdrawMoney()
-        {
-            Console.WriteLine("SBI Bank Withdraw Money");
-        }
-    }
-
-    public class AXIX : IBank
-    {
-        public override void BankTransfer()
-        {
-            Console.WriteLine("AXIX Bank Bank Transfer");
-        }
-
-        public override void CheckBalance()
-        {
-            Console.WriteLine("AXIX Bank Check Balance");
-        }
-
-        public override void MiniStatement()
-        {
-            Console.WriteLine("AXIX Bank Mini Statement");
-        }
-
-        public override void ValidateCard()
-        {
-            Console.WriteLine("AXIX Bank Validate Card");
-        }
-
-        public override void WithdrawMoney()
-        {
-            Console.WriteLine("AXIX Bank Withdraw Money");
-        }
-    }
-    class Program
+    class Program : A
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Transaction doing SBI Bank");
-            //            SBI sBI = new SBI();
-            IBank sBI = BankFactory.GetBankObject("SBI");
-            sBI.ValidateCard();
-            sBI.WithdrawMoney();
-            sBI.CheckBalance();
-            sBI.BankTransfer();
-            sBI.MiniStatement();
-
-            Console.WriteLine("");
-
-            Console.WriteLine("Transaction doing AXIX Bank");
-            //            AXIX aXIX = new AXIX();
-            IBank aXIX = BankFactory.GetBankObject("AXIX");
-            aXIX.ValidateCard();
-            aXIX.WithdrawMoney();
-            aXIX.CheckBalance();
-            aXIX.BankTransfer();
-            aXIX.MiniStatement();
-
+            Program program = new Program();
+            program.Method1();
+            program.Method2();
             Console.Read();
         }
     }
