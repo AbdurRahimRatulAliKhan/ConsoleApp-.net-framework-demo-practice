ebf4e506d441ee5b8b39c20499ed0be554652fac
AbdurRahimRatulAliKhan <rahim498617@gmail.com>
2023-01-11 01:43:30 +0600

    implicitly called constructor

--- ConsoleApp .net framework demo practice/Program.cs
+++ ConsoleApp .net framework demo practice/Program.cs
@@ -5,6 +5,10 @@ namespace ConsoleApp.net_framework_demo_practice
 {
     class A
     {
+        public A()
+        {
+            Console.WriteLine("Class A constructor is called!");
+        }
         public void Method1()
         {
             Console.WriteLine("Method 1");
@@ -16,6 +20,10 @@ namespace ConsoleApp.net_framework_demo_practice
     }
     class Program : A
     {
+        public Program()
+        {
+            Console.WriteLine("class program constructor is called!");
+        }
         public void Method3()
         {
             Console.WriteLine("Method 3");
